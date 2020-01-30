a0e077b38a889e081ad3454ced431594e2a58ce8
sergio <vedrunainfo@gmail.com>
2020-01-30 10:10:10 +0100

    primer commit

---
+++ src/variables/Coche.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Coche {
+		//ATRIBUTOS
+		int numruedas;
+		//int numpuertas;
+		public int numpuertas;
+		//COMENTARIOS
+		public Coche() {}
+		public Coche(int numruedas, int numpuertas) {
+		
+			this.numruedas = numruedas; 
+			this.numpuertas = numpuertas;
+		}
+
+
+
+
+
+
+
+
+}
---
+++ src/variables/Main.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int var1;
+		var1=5;
+		
+		int var2;
+		var2=6;
+		
+		float var3;
+		var3=5.6F;
+		
+		String string1;
+		string1= "hola";
+		
+		Coche coche1;
+		coche1 = new Coche();
+		coche1.numpuertas = 5;
+	
+	}
+
+}
